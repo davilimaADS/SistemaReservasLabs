3ca2239eb8bbf425b2b6ddc0a9c6a3e3fad62f10
davilimaADS <daviqueirozlima6@gmail.com>
2025-06-09 05:52:31 -0300

    :sparkles: feat: initial implementation endpoint for users

---
+++ SistemaReservasLabs/Controllers/UserController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaReservasLabs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+    }
+}
